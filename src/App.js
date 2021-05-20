e08ba1d8d88587bddb04206830baaf4edaab75a4
McKynlee <mckynlee.westman@gmail.com>
2021-05-20 10:43:11 -0500

    converted to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -14,8 +14,8 @@ class App extends Component {
             Ricotta fromage jarlsberg stinking bishop boursin babybel queso parmesan.
             Emmental taleggio queso taleggio cheese and biscuits everyone loves stinking bishop cheese triangles.
             Bocconcini the big cheese cow bavarian bergkase port-salut when the cheese comes out everybody's happy fondue cheesecake.
-            Babybel blue castello hard cheese cream cheese ricotta pecorino croque monsieur pecorino. 
-            Goat cheesy grin croque monsieur caerphilly swiss bavarian bergkase ricotta croque monsieur. 
+            Babybel blue castello hard cheese cream cheese ricotta pecorino croque monsieur pecorino.
+            Goat cheesy grin croque monsieur caerphilly swiss bavarian bergkase ricotta croque monsieur.
             Danish fontina cheese and biscuits.
           </div>
           <div id="subMain">
@@ -29,24 +29,24 @@ class App extends Component {
               <button>Submit</button>
             </div>
             <ul>
-              <li><img src="http://www.fillmurray.com/200/250"/></li>
-              <li><img src="http://www.fillmurray.com/300/250"/></li>
-              <li><img src="http://www.fillmurray.com/50/50"/></li>
-              <li><img src="http://www.fillmurray.com/200/100"/></li>
-              <li><img src="http://www.fillmurray.com/50/100"/></li>
-              <li><img src="http://www.fillmurray.com/100/100"/></li>
-              <li><img src="http://www.fillmurray.com/300/350"/></li>
-              <li><img src="http://www.fillmurray.com/400/450"/></li>
-              <li><img src="http://www.fillmurray.com/80/80"/></li>
-              <li><img src="http://www.fillmurray.com/260/300"/></li>
-              <li><img src="http://www.fillmurray.com/80/150"/></li>
-              <li><img src="http://www.fillmurray.com/130/90"/></li>
+              <li><img src="http://www.fillmurray.com/200/250" /></li>
+              <li><img src="http://www.fillmurray.com/300/250" /></li>
+              <li><img src="http://www.fillmurray.com/50/50" /></li>
+              <li><img src="http://www.fillmurray.com/200/100" /></li>
+              <li><img src="http://www.fillmurray.com/50/100" /></li>
+              <li><img src="http://www.fillmurray.com/100/100" /></li>
+              <li><img src="http://www.fillmurray.com/300/350" /></li>
+              <li><img src="http://www.fillmurray.com/400/450" /></li>
+              <li><img src="http://www.fillmurray.com/80/80" /></li>
+              <li><img src="http://www.fillmurray.com/260/300" /></li>
+              <li><img src="http://www.fillmurray.com/80/150" /></li>
+              <li><img src="http://www.fillmurray.com/130/90" /></li>
             </ul>
           </div>
         </main>
         <div id="footer">
           <p id="footerCopy">
-            Macaroni cheese danish fontina stilton cheddar say cheese cheese on toast say cheese cottage cheese. 
+            Macaroni cheese danish fontina stilton cheddar say cheese cheese on toast say cheese cottage cheese.
           </p>
           <div id="contactInfo">
             <p>Casie Siekman</p>
